0c79d13b1b2f1426ede0bc394cb96c60458b3cca
miihhael <mikael21003@student.hamk.fi>
2024-02-09 01:30:13 +0200

    Ensimmäinen commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AvaaIkkuna(object sender, RoutedEventArgs e)
+        {
+            Etusivu objEtusivu = new Etusivu();
+            objEtusivu.Show();
+        }
+
+
+
+        private void AvaaIkkuna1(object sender, RoutedEventArgs e)
+        {
+            Sivuvalikko objSivuvalikko = new Sivuvalikko();
+            objSivuvalikko.Show();
+        }
+    }
+}
