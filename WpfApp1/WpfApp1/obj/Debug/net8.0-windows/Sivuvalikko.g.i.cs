74ca644706d92d763150a342c24f3dd7d8307cca
miihhael <mikael21003@student.hamk.fi>
2024-02-09 01:39:25 +0200

    Etusivu & Sivuvalikko

--- WpfApp1/WpfApp1/obj/Debug/net8.0-windows/Sivuvalikko.g.i.cs
+++ WpfApp1/WpfApp1/obj/Debug/net8.0-windows/Sivuvalikko.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/sivuvalikko.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/sivuvalikko.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Sivuvalikko.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58cff60a5358c3e47301e22a14a0e161a5d925d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
