808f070f9da77be59cba03a048293b7ba26dfed8
miihhael <mikael21003@student.hamk.fi>
2024-02-09 02:02:53 +0200

    JOJOJOJO

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IKKUNA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
