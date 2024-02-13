04824b3a20c64a66962cee485944c1ec827cd770
fei <fei.raita@hotmail.com>
2024-02-13 12:17:34 +0200

    Popup -ikkuna lisätty, ilmoitus menee Etusivu -ikkunan ulkopuolelle

--- Rahatraiteille/Rahatraiteille/Etusivu.xaml.cs
+++ Rahatraiteille/Rahatraiteille/Etusivu.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Rahatraiteille
 {
@@ -27,6 +28,33 @@ namespace Rahatraiteille
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += dtTicker;
+            dt.Start();
+        }
+        private int increment = 0;
+        private async void dtTicker(object sender, EventArgs e)
+        {
+            increment++;
+
+            TimerLable.Content = increment.ToString();
+
+            if (increment == 7)
+            {
+                Popup1.IsOpen = true;
+                increment = 0;
+            }
+            if (increment == 1)
+            {
+                await Task.Delay(3500);
+                Popup1.IsOpen = false;
+                Popup1.PopupAnimation = System.Windows.Controls.Primitives.PopupAnimation.Fade;
+            }
         }
     }
 }
