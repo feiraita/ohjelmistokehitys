660f6611becec3eafc8a2065ef53dab01eeb6f44
fei <fei.raita@hotmail.com>
2024-02-15 15:36:31 +0200

    Lisää Kirjaus sivun täyttö ja classin luonti

---
+++ Rahatraiteille/Rahatraiteille/Kirjaus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rahatraiteille
+{
+    internal class Kirjaus
+    {
+        public string nimi { get; set; }
+        public double euro { get; set; }
+
+        public Kirjaus(string nimi)
+        {
+            this.nimi = nimi;
+            this.euro = euro;
+        }
+    }
+}
--- Rahatraiteille/Rahatraiteille/LisaaKirjaus-sivu.xaml.cs
+++ Rahatraiteille/Rahatraiteille/LisaaKirjaus-sivu.xaml.cs
@@ -23,5 +23,45 @@ namespace Rahatraiteille
         {
             InitializeComponent();
         }
+  
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = string.Empty;
+            double cost;
+
+            if (!string.IsNullOrEmpty(nimiTextBox.Text)) name = nimiTextBox.Text;
+
+            double.TryParse(euroTextBox.Text, out cost);
+
+            if (!string.IsNullOrEmpty(name) && cost > 0)
+            {
+                
+            }
+
+            nimiTextBox.Text = "";
+            euroTextBox.Text = "";
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (nimiTextBox.Text != "")
+            {
+                nimiPlaceholder.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                nimiPlaceholder.Visibility = Visibility.Visible;
+            }
+
+            if (euroTextBox.Text != "")
+            {
+                euroPlaceholder.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                euroPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
